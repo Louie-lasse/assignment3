0e0c0744b8ba47c952b6188aabbdbc8e8146d550
Louie-lasse <love.lindqvist101@gmail.com>
2022-02-22 17:35:06 +0100

    fixes misspelling

--- tst.sql
+++ tst.sql
@@ -1,2 +1,2 @@
-INSERT INTO Registration VALUES (3333333333,'CCC333');
-INSERT INTO Registration VALUES (4444444444,'CCC333');
+INSERT INTO Registrations VALUES (3333333333,'CCC333'); -- this student has already passed
+INSERT INTO Registrations VALUES (4444444444,'CCC333'); -- the course if full
