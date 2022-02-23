5c5060be54866107a0e9ba87afd798edacc6a8e9
Louie-lasse <love.lindqvist101@gmail.com>
2022-02-23 11:29:45 +0100

    adds tests for adding in waitinglist

--- tst.sql
+++ tst.sql
@@ -1,3 +1,4 @@
 INSERT INTO Registrations VALUES (3333333333,'CCC333'); -- this student has already passed
 INSERT INTO Registrations VALUES (4444444444,'CCC333'); -- the course if full
 INSERT INTO Registrations VALUES (1111111111,'CCC555'); -- should insert into course
+INSERT INTO Registrations VALUES (1111111111,'CCC222'); -- should insert into waitinglist
