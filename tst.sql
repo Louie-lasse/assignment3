fef1ff48914df9b53be7e879856e9183997a2b50
Louie-lasse <love.lindqvist101@gmail.com>
2022-03-07 11:56:36 +0100

    builds preliminary Query

--- tst.sql
+++ tst.sql
@@ -6,11 +6,12 @@ DELETE FROM Registrations WHERE idnr='1111111111' AND course='CCC111';
 DELETE FROM Registrations WHERE idnr='1111111111' AND course='CCC222';
 DELETE FROM Registrations WHERE idnr='1111111111' AND course='CCC333';
 
-SELECT idnr,name,login,program,branch,array_agg(json_build_object('course',T.course,'grade',grade)) finnished,
-       array_agg(R.course) registered,
-       seminarcourses,mathcredits,researchcredits,totalCredits,qualified AS canGraguate
+SELECT json_build_object('idnr',idnr,'name',name,'login',login,'program',program,'branch',branch,
+    'finished',array_agg(json_build_object('course',T.course,'grade',grade)),
+    'registered',array_agg(R.course), 'seminarcourses',seminarcourses,'mathcredits',mathcredits,
+    'researchcredits',researchcredits,'totalCredits',totalCredits,'canGraguate',qualified)
 FROM BasicInformation B
 JOIN PathToGraduation P ON B.idnr=P.student
 FULL OUTER JOIN Taken T ON P.student=T.student
 FULL OUTER JOIN Registrations R ON R.student = B.idnr
-GROUP BY (idnr,name,login,program,branch,seminarcourses,mathcredits,researchcredits,totalCredits,canGraguate);
+GROUP BY (idnr,name,login,program,branch,seminarcourses,mathcredits,researchcredits,totalCredits,qualified);
