5c3d777527216eaa7908388ed35f6a60c2e8cb86
Louie-lasse <love.lindqvist101@gmail.com>
2022-03-08 14:32:26 +0100

    Fixes almost everything

--- PortalConnection.java
+++ PortalConnection.java
@@ -68,17 +68,26 @@ public class PortalConnection {
     public String getInfo(String student) throws SQLException{
         
         try(PreparedStatement st = conn.prepareStatement(
-            // replace this with something more useful
-            "SELECT json_build_object('idnr',idnr,'name',name,'login',login,'program',program,'branch',branch, "+
-                    "'finished',array_agg(json_build_object('course',T.course,'grade',grade)), "+
-                    "'registered',array_agg(R.course), 'seminarcourses',seminarcourses,'mathcredits',mathcredits, "+
-                    "'researchcredits',researchcredits,'totalCredits',totalCredits,'canGraguate',qualified) jsondata " +
-                "FROM BasicInformation B " +
-                "FULL OUTER JOIN PathToGraduation P ON B.idnr=P.student " +
-                "FULL OUTER JOIN Taken T ON P.student=T.student " +
-                "FULL OUTER JOIN Registrations R ON R.student = B.idnr " +
-                "WHERE B.idnr=? " +
-                "GROUP BY (idnr,name,login,program,branch,seminarcourses,mathcredits,researchcredits,totalCredits,qualified) "
+            // replace this with something more usef
+                "WITH T AS ( " +
+                        "SELECT student, name, code, grade, credits FROM Taken " +
+                        "JOIN Courses ON course = code " +
+                "), R AS ( " +
+                        "SELECT R.student, name, code, status, place AS position FROM Registrations R " +
+                        "JOIN Courses ON R.course = code " +
+                        "FULL OUTER JOIN CourseQueuePositions Q ON code = Q.course AND R.student = Q.student " +
+                ") " +
+                "SELECT json_build_object('idnr',idnr,'name',B.name,'login',login,'program',program,'branch',branch, " +
+                        "'finished',array_agg(json_build_object('course',T.name,'code',T.code,'credits',T.credits,'grade',grade)), " +
+                        "'registered',array_agg(json_build_object('course',R.name,'code',R.code,'status',R.status,'position',R.position)), " +
+                        "'seminarcourses',seminarcourses,'mathcredits',mathcredits, " +
+                        "'researchcredits',researchcredits,'totalCredits',totalCredits,'canGraduate',qualified) jsondata " +
+                "FROM T " +
+                "FULL OUTER JOIN BasicInformation B ON B.idnr=T.student " +
+                "JOIN PathToGraduation P ON B.idnr=P.student " +
+                "FULL OUTER JOIN R ON R.student = B.idnr " +
+                "WHERE B.idnr = ? " +
+                "GROUP BY (idnr,B.name,login,program,branch,seminarcourses,mathcredits,researchcredits,totalCredits,qualified) "
 
             )){
             
--- tst.sql
+++ tst.sql
@@ -6,15 +6,24 @@ DELETE FROM Registrations WHERE idnr='1111111111' AND course='CCC111';
 DELETE FROM Registrations WHERE idnr='1111111111' AND course='CCC222';
 DELETE FROM Registrations WHERE idnr='1111111111' AND course='CCC333';
 
-SELECT json_build_object('idnr',idnr,'name',name,'login',login,'program',program,'branch',branch,
-    'finished',array_agg(json_build_object('course',T.course,'grade',grade)),
-    'registered',array_agg(R.course), 'seminarcourses',seminarcourses,'mathcredits',mathcredits,
-    'researchcredits',researchcredits,'totalCredits',totalCredits,'canGraguate',qualified)
-FROM BasicInformation B
+WITH T AS (
+    SELECT student, name, code, grade, credits FROM Taken
+    JOIN Courses ON course = code
+), R AS (
+    SELECT R.student, name, code, status, place AS position FROM Registrations R
+    JOIN Courses ON R.course = code
+    FULL OUTER JOIN CourseQueuePositions Q ON code = Q.course AND R.student = Q.student
+)
+SELECT json_build_object('idnr',idnr,'name',B.name,'login',login,'program',program,'branch',branch,
+    'finished',array_agg(json_build_object('course',T.name,'code',T.code,'credits',T.credits,'grade',grade)),
+    'registered',array_agg(json_build_object('course',R.name,'code',R.code,'status',R.status,'position',R.position)),
+    'seminarcourses',seminarcourses,'mathcredits',mathcredits,
+    'researchcredits',researchcredits,'totalCredits',totalCredits,'canGraguate',qualified) jsondata
+FROM T
+FULL OUTER JOIN BasicInformation B ON B.idnr=T.student
 JOIN PathToGraduation P ON B.idnr=P.student
-FULL OUTER JOIN Taken T ON P.student=T.student
-FULL OUTER JOIN Registrations R ON R.student = B.idnr
-GROUP BY (idnr,name,login,program,branch,seminarcourses,mathcredits,researchcredits,totalCredits,qualified);
+FULL OUTER JOIN R ON R.student = B.idnr
+GROUP BY (idnr,B.name,login,program,branch,seminarcourses,mathcredits,researchcredits,totalCredits,qualified);
 
 
 /*
